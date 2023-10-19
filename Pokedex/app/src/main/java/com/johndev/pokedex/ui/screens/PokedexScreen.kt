8a8e83b8e205840573814ba3470f36cc966957a9
John Morales <johnta2610@hotmail.com>
2023-10-19 12:15:59 -0600

    ⭐Adicion del LazyVerticalGrid

--- Pokedex/app/src/main/java/com/johndev/pokedex/ui/screens/PokedexScreen.kt
+++ Pokedex/app/src/main/java/com/johndev/pokedex/ui/screens/PokedexScreen.kt
@@ -2,56 +2,52 @@ package com.johndev.pokedex.ui.screens
 
 import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.johndev.pokedex.R
 import com.johndev.pokedex.common.entities.PokemonEntity
-import com.johndev.pokedex.common.entities.Sprites
-import com.johndev.pokedex.common.entities.Type
 import com.johndev.pokedex.common.utils.Retrofit.service
 import com.johndev.pokedex.common.utils.capitalizeFirstLetter
 import com.johndev.pokedex.common.utils.formatId
 import com.johndev.pokedex.common.utils.getImageById
-import com.johndev.pokedex.ui.theme.PokedexTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -59,15 +55,15 @@ fun PokedexScreen() {
     val pokemonListState = remember { mutableStateOf<List<PokemonEntity>?>(null) }
     val tempList = mutableListOf<PokemonEntity>()
     val offset = remember { mutableStateOf(0) }
-    val limit = remember { mutableStateOf(10) }
+    val limit = remember { mutableStateOf(50) }
     // Realiza la solicitud de los 10 primeros Pokemon
     for (index in offset.value..limit.value) {
         LaunchedEffect(index) {
             try {
                 val response = service.getPokemonInfo(index.toString())
                 tempList.add(response)
-                if (tempList.size == 10) {
-                    pokemonListState.value = tempList.toList().sortedByDescending { it.id }
+                if (tempList.size == 50) {
+                    pokemonListState.value = tempList.toList().sortedBy { it.id }
                 }
             } catch (e: Exception) {
                 Log.e("PokedexScreen", "Error: $e")
@@ -87,10 +83,19 @@ fun PokedexScreen() {
                     .padding(paddingValues)
             ) {
                 MainContent(pokemonListState.value) {
-                    offset.value += 10
-                    limit.value += 10
+                    offset.value += 50
+                    limit.value += 50
                     pokemonListState.value = null
                 }
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                offset.value += 50
+                limit.value += 50
+                pokemonListState.value = null
+            }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
             }
         }
     )
@@ -107,12 +112,21 @@ fun MainContent(listPokemon: List<PokemonEntity>?, callback: () -> Unit) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(horizontal = 16.dp, vertical = 8.dp)
-                .verticalScroll(rememberScrollState())
+            //.verticalScroll(rememberScrollState())
         ) {
-            listPokemon.forEach {
+            LazyVerticalGrid(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                columns = GridCells.Fixed(2),
+                content = {
+                items(listPokemon.size) {
+                    CardPokemonItemGrid(pokemonEntity = listPokemon[it])
+                }
+            })
+            /*listPokemon.forEach {
                 CardPokemonItem(it)
                 Spacer(modifier = Modifier.height(8.dp))
-            }
+            }*/
             /*LazyColumn(
                 modifier = Modifier
                     .fillMaxWidth(),
@@ -122,16 +136,36 @@ fun MainContent(listPokemon: List<PokemonEntity>?, callback: () -> Unit) {
                         CardPokemonItem(listPokemon[it])
                     }
                 })*/
-            Button(onClick = { callback() }) {
-                Text(text = "Prueba")
-            }
         }
     }
 }
 
 @Composable
-fun CardPokemonItemGrid() {
-
+fun CardPokemonItemGrid(pokemonEntity: PokemonEntity) {
+    Box(
+        modifier = Modifier
+            .border(width = 1.dp, color = Color.LightGray, shape = RoundedCornerShape(8.dp))
+            .size(100.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = formatId(pokemonEntity.id))
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                model = getImageById(pokemonEntity.id),
+                contentDescription = null,
+                contentScale = ContentScale.Fit
+            )
+            Text(text = pokemonEntity.name.capitalizeFirstLetter())
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
